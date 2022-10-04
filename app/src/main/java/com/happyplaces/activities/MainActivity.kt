a0dc0c67d2c42af7c85f8a36425a83b1e3739bba
kkk406032008 <62056209+kkk406032008@users.noreply.github.com>
2022-10-04 19:51:33 +0800

    Initial commit

---
+++ app/src/main/java/com/happyplaces/activities/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.happyplaces.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.happyplaces.R
+import com.happyplaces.adapters.HappyPlacesAdapter
+import com.happyplaces.database.DatabaseHandler
+import com.happyplaces.models.HappyPlaceModel
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_main)
+
+        // Setting an click event for Fab Button and calling the AddHappyPlaceActivity.
+        fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
+        getHappyPlacesListFromLocalDB()
+    }
+
+    /**
+     * A function to get the list of happy place from local database.
+     */
+    private fun getHappyPlacesListFromLocalDB() {
+
+        val dbHandler = DatabaseHandler(this)
+
+        val getHappyPlacesList = dbHandler.getHappyPlacesList()
+
+        // TODO (Step 8: Calling an function which have created for getting list of inserted data from local database
+        //  and passing the list to recyclerview to populate in UI.)
+        // START
+        if (getHappyPlacesList.size > 0) {
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlacesList)
+        } else {
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
+        }
+        // END
+    }
+
+    // TODO(Step 7 : Creating a function for setting up the recyclerview to UI.)
+    // START
+    /**
+     * A function to populate the recyclerview to the UI.
+     */
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>) {
+
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
+        rv_happy_places_list.adapter = placesAdapter
+    }
+    // END
+}
